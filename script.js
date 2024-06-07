8885a3ff257db2fa4864df6448163ed6b1b6f032
7minutech <elijohnson1107@gmail.com>
2024-06-07 18:11:35 -0500

    Added comment to getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice() {
     // generates number between 1 and 3
+    // returns rock paper or scissors
     let rnum = Math.floor(Math.random() * 3) + 1
     console.log(rnum)
     if(rnum == 1){
